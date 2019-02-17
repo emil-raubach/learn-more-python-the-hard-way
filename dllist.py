969550b5939b70a2bd5d2273da6d7f72525a12c8
Emil Raubach <eraubach@gmail.com>
2019-02-17 16:01:07 -0700

    DLL stub functions

--- dllist.py
+++ dllist.py
@@ -18,3 +18,49 @@ class DoubleLinkedList(object):
     def __init__(self):
         self.begin = None
         self.end = None
+
+
+    def push(self, obj):
+        """Appends a new value on the end of the list"""
+
+    
+    def pop(self):
+        """Removes the last item and returns it."""
+
+
+    def shift(self, obj):
+        """Actually just another name for push"""
+
+
+    def unshift(self):
+        """Removes the first item (from begin) and returns it."""
+
+
+    def detach_node(self, node):
+        """You'll need to use this operation sometimes, but mostly
+        inside remove().  It should take a node, and detach it from 
+        the list, whether the node is at the front, end, or in the middle."""
+
+
+    def remove(self, obj):
+        """Finds a matching item and removes it from the list."""
+
+
+    def first(self):
+        """Returns a *reference* to the first item, does not remove."""
+
+
+    def last(self):
+        """Returns a reference to the last item, does not remove."""
+
+
+    def count(self):
+        """Counts the number of elements in the list."""
+
+
+    def get(self, index):
+        """Get the value at index."""
+
+
+    def dump(self, mark):
+        """Debugging function that dumps the contents of the list.""""
