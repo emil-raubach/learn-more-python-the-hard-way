2a89263fd555ef6748b010874248592884c2a589
Emil Raubach <eraubach@gmail.com>
2019-02-22 13:26:30 -0700

    Added initial test code for sorting.

---
+++ ex16/test_sorting.py
@@ -0,0 +1,38 @@
+import sorting
+from ex14.dllist import DoubleLinkedList
+from random import randint
+
+max_numbers = 30
+
+def random_list(count):
+    numbers = DoubleLinkedList()
+    for i in range(count, 0, -1):
+        numbers.shift(randint(0, 10000))
+    return numbers
+
+
+def is_sorted(numbers):
+    node = numbers.begin
+    while node and node.next:
+        if node.value > node.next.value:
+            return False
+        else:
+            node = node.next
+
+    return True
+
+
+def test_bubble_sort():
+    numbers = random_list(max_numbers)
+
+    sorting.bubble_sort(numbers)
+
+    assert is_sorted(numbers)
+
+
+# def test_merge_sort():
+#     numbers = random_list(max_numbers)
+
+#     sorting.merge_sort(numbers)
+
+#     assert is_sorted(numbers)
