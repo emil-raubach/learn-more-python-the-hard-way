62366d1eb8de31167623a7b5dae4f72040bc4028
Emil Raubach <eraubach@gmail.com>
2019-02-21 21:52:49 -0700

    Zed's implementation of bubble sort

---
+++ ex16/bubble_sort.py
@@ -0,0 +1,18 @@
+def bubble_sort(numbers):
+    """Sorts a list of numbers using bubble sort."""
+    while True:
+        # start off assuming it's sorted
+        is_sorted = True
+        # comparing 2 at a time, skipping ahead
+        node = numbers.begin.next
+        while node:
+            # loop through comparing node to the next
+            if node.prev.value > node.value:
+                # if the next is greater, then we need to swap
+                node.prev.value, node.value = node.value, node.prev.value
+                # oops, looks like we have to scan again
+                is_sorted = False
+            node = node.next
+
+        # this is reset at the top but if we never swapped then it's sorted
+        if is_sorted: break
