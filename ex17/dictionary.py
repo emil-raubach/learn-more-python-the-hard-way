0c8990ddb7146c6afbe1fdb7278a7ee8a533e45f
Emil Raubach <eraubach@gmail.com>
2019-03-20 20:21:10 -0600

    Dictionary class with debug print statements included.

--- ex17/dictionary.py
+++ ex17/dictionary.py
@@ -3,43 +3,58 @@ from dllist import DoubleLinkedList
 class Dictionary(object):
     def __init__(self, num_buckets=256):
         """Initializes a Map with the given number of buckets."""
-        self.map = DoubleLinkedList()
+        self.map = DoubleLinkedList() # create new dllist
+        # Create a new dllist for each bucket in the map
         for i in range(0, num_buckets):
             self.map.push(DoubleLinkedList())
-
+    
     def hash_key(self, key):
         """Given a key this will create a number and then convert it to
         an index for the aMap's buckets."""
+        # uses a key to get a unique integer, modulus to get the index
         return hash(key) % self.map.count()
     
     def get_bucket(self, key):
         """Given a key, find the bucket where it would go."""
+        # gets index for a key and assigns it to a bucket
         bucket_id = self.hash_key(key)
-        return self.map.get(bucket_id)
+        print("> enter get_bucket() - key=", key, "bucket_id=", bucket_id)
+        # uses the index to get the value of the bucket (which is a dllist node)
+        return self.map.get(bucket_id) # calls the dllist get() function! OMG!!
 
     def get_slot(self, key, default=None):
         """
         Returns either the bucket and node for a slot, or None, None
         """
-        bucket = self.get_bucket(key)
-
+        print(">> enter get_slot() - key=", key)
+        # gets ref to the slot dllist for a given key
+        bucket = self.get_bucket(key) # slot not bucket right?
+        print(">> get_slot() after get_bucket() - key=", key, "bucket=", bucket)
+        # if the slot in the bucket is not None
         if bucket:
-            node = bucket.begin
+            # initialize to first node in the slot dllist
+            node = bucket.begin # set to first node
             i = 0
-
+            # traverse the slot dllist & check for keys existance
             while node:
+                # if the key matches the first element of the key/value tuple
                 if key == node.value[0]:
-                    return bucket, node
+                    print("<< leave get_slot() if key==node.value - bucket=", bucket, "node=", node)
+                    return bucket, node # return the bucket and a ref to the slot / node
                 else:
-                    node = node.next
-                    i += 1
-
+                    node = node.next 
+                    i += 1 # not sure what this is for...
+        print("<< leave get_slot() if fall through - bucket=", bucket)
         # fall through for both if and while above
-        return bucket, None
+        return bucket, None # if key isn't already in the list
 
     def get(self, key, default=None):
         """Gets the value in a bucket for the given key, or the default."""
+        print(">>> enter get() - key=", key)
+        # gets the bucket and slot for a given key
         bucket, node = self.get_slot(key, default=default)
+        print(">>> in get(), after get_slot() - key=", key, "bucket=", bucket, "node=", node, "node.value=", node.value)
+        # 
         return node and node.value[1] or default
 
     def set(self, key, value):
--- ex17/test_dictionary.py
+++ ex17/test_dictionary.py
@@ -1,57 +1,63 @@
 from dictionary import Dictionary
 
 # create a mapping of state to abbreviation
-states = Dictionary()
-states.set('Oregon', 'OR')
-states.set('Florida', 'FL')
-states.set('California', 'CA')
-states.set('New York', 'NY')
-states.set('Michigan', 'MI')
+def test_set():
+    states = Dictionary()
+    states.set('Oregon', 'OR')
+    states.set('Florida', 'FL')
+    states.set('California', 'CA')
+    states.set('New York', 'NY')
+    states.set('Michigan', 'MI')
+    assert states.get('Oregon') == 'OR'
+    assert states.get('Florida') == 'FL'
+    assert states.get('California') == 'CA'
+    assert states.get('New York') == 'NY'
+    assert states.get('Michigan') == 'MI'
 
-# create a basic set of states and some cities in them
-cities = Dictionary()
-cities.set('CA', 'San Fransico')
-cities.set('MI', 'Detriot')
-cities.set('FL', 'Jacksonville')
+    # create a basic set of states and some cities in them
+    cities = Dictionary()
+    cities.set('CA', 'San Fransico')
+    cities.set('MI', 'Detriot')
+    cities.set('FL', 'Jacksonville')
 
-# add some more cities
-cities.set('NY', 'New York')
-cities.set('OR', 'Portland')
+    # add some more cities
+    cities.set('NY', 'New York')
+    cities.set('OR', 'Portland')
 
 
 # print out some cities
-print('-' * 10)
-print("NY State has: %s" % cities.get('NY'))
-print("OR State has: %s" % cities.get('OR'))
+# print('-' * 10)
+# print("NY State has: %s" % cities.get('NY'))
+# print("OR State has: %s" % cities.get('OR'))
 
-# print some states
-print('-' * 10)
-print("Michigan's abbreviation is: %s" % states.get('Michigan'))
-print("Florida's abbreviation is: %s" % states.get('Florida'))
+# # print some states
+# print('-' * 10)
+# print("Michigan's abbreviation is: %s" % states.get('Michigan'))
+# print("Florida's abbreviation is: %s" % states.get('Florida'))
 
-# do it by using the state then cities dict
-print('-' * 10)
-print("Michigan has: %s" % cities.get(states.get('Michigan')))
-print("Florida has: %s" % cities.get(states.get('Florida')))
+# # do it by using the state then cities dict
+# print('-' * 10)
+# print("Michigan has: %s" % cities.get(states.get('Michigan')))
+# print("Florida has: %s" % cities.get(states.get('Florida')))
 
-# print every state abbreviation
-print('-' * 10)
-states.list()
+# # print every state abbreviation
+# print('-' * 10)
+# states.list()
 
-# print every city in state
-print('-' * 10)
-cities.list()
+# # print every city in state
+# print('-' * 10)
+# cities.list()
 
-print('-' * 10)
-state = states.get('Texas')
+# print('-' * 10)
+# state = states.get('Texas')
 
-if not state:
-    print("Sorry, no Texas.")
+# if not state:
+#     print("Sorry, no Texas.")
 
-# can you do this on one line?
-city = cities.get('TX', 'Does Not Exist')
-print("The city for the state 'TX' is: %s" % city)
+# # can you do this on one line?
+# city = cities.get('TX', 'Does Not Exist')
+# print("The city for the state 'TX' is: %s" % city)
 
-cities.delete('FL')
-city = cities.get('FL', 'Does Not Exist')
-print("FL should be gone: %r" % city)
+# cities.delete('FL')
+# city = cities.get('FL', 'Does Not Exist')
+# print("FL should be gone: %r" % city)
