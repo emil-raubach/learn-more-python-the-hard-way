5d6f0482866e191322fad69183e4432d12a2f155
Emil Raubach <eraubach@gmail.com>
2019-06-21 19:44:08 -0600

    changed the BSTreeNode repr to match Zed.

--- ex20/bstree.py
+++ ex20/bstree.py
@@ -6,7 +6,7 @@ class BSTreeNode(object):
         self.left = left
         self.right = right
         self.parent = parent
-
+    # This seems to lead to a recursion error when I access `.root`
     def __repr__(self):
         return (f"{self.__class__.__name__}({self.key!r}, {self.value!r}, "
                 f"{self.left!r}, {self.right!r}, {self.parent!r})") 
@@ -21,22 +21,24 @@ class BSTree(object):
 
     def get(self, key):
         """Return the value of a node with the given key, or return None."""
-        
+        # are we comparing the keys or the values?  Values makes more sense...
         # start at the root
+        # if self.root is not None:
+            # if node.key = node.value (not value but key right?)
+                # return node.value
 
-        # if node.key = node.value (not value but key right?)
-            # return node.value
-
-        # if no left and right child:
+            # if no left and right child:
+                # return None
+            # elif key < node key:
+                # You go left if the given key is less-than the node's key. 
+                #   node = node.left
+            # else:
+                # You go right if the key is greater-than the node's key. 
+                # node = node.right
+                
             # return None
-        # elif key < node key:
-            # You go left if the given key is less-than the node's key. 
-            #   node = node.left
         # else:
-            # You go right if the key is greater-than the node's key. 
-            # node = node.right
-            
-        # return None
+            # return None
         pass
 
 
@@ -48,7 +50,7 @@ class BSTree(object):
             node = self.root
 
             while True:    
-                if key < node.key:                    
+                if key <= node.key:                    
 
                     if node.left:
                         node = node.left
@@ -57,7 +59,7 @@ class BSTree(object):
                         node.left = new_node
                         self.num_nodes += 1
                         break
-                elif key >= node.key:
+                elif key > node.key:
                     if node.right:
                         node = node.right
                     else:
@@ -66,7 +68,7 @@ class BSTree(object):
                         self.num_nodes += 1
                         break
                 else:
-                    pass # what goes here
+                    break # what goes here
         else:
             self.root = BSTreeNode(key, value, None, None, None)
             self.num_nodes += 1        
@@ -80,3 +82,19 @@ class BSTree(object):
     def list(self):
         """Print out the contents of the list."""
         pass
+        # Walk the tree and print everything out. 
+        # The important piece to list is that you can walk the tree in different ways to produce different output. 
+        # If you walk the left, then the right paths, you get something different than if you do the inverse. 
+        # If you go all the way to the bottom and then print as you come up the tree toward root, you get yet another kind of output. 
+        # You can also print the nodes as you go down the tree, from root to the "leaves". 
+        # Try different styles to see which one does what.
+        cur = self.root
+
+        while cur:
+            pass
+
+
+    def _invariant(self):
+        # root node parent should always be None
+        if self.root:
+            assert self.root.parent is None, "root.parent not None"
--- ex20/test_bstree.py
+++ ex20/test_bstree.py
@@ -2,7 +2,10 @@ from bstree import BSTree
 
 def test_get():
     numbers = BSTree()
-    numbers.set('one', 1)
+    numbers.set(1, 'one')
+    numbers._invariant()
+    numbers.set(2, 'two')
+    numbers.set(3, 'three')
     # assert numbers.get('one') == 1
 
 
