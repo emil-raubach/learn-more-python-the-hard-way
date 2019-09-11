d877583ae029fce11d602dbcc27330859c87e154
Emil Raubach <eraubach@gmail.com>
2019-09-10 19:38:44 -0600

    Start of ex26; didn't really get ex25 implemented with other options.

--- ex25/ex25_1.py
+++ ex25/ex25_1.py
@@ -9,22 +9,47 @@ def parse_args():
     parser = argparse.ArgumentParser(
         description='utility that emulates `xargs`')
     parser.add_argument('command', help='*nix command for `xargs` to execute.')
+    parser.add_argument('-p', '--prompt', 
+                        help='prompt user before executing command.', action='store_true')
+
     return parser.parse_args()
 
-def xargs(args_list):
+def xargs(execute):
+    # cmd = args_list.command
+    
+    # for line in sys.stdin.readlines():
+    #     exec_ = [cmd] + [line.strip()]
+    #     completed_proc = subprocess.run(exec_)
+    completed_proc = subprocess.run(execute)
+
+def get_args(args_list):
     cmd = args_list.command
     
     for line in sys.stdin.readlines():
         exec_ = [cmd] + [line.strip()]
-        completed_proc = subprocess.run(exec_)
+
+    return exec_
 
 
 def main():
     args = parse_args()
+    ex = get_args(args)
     
-    print(">>> parsed args:  ", args)
+    # if the `-p` flag is True
+    if args.prompt:
+    # Get ok from user before each call to xargs
+        for cmd in ex:
+            print(f'{cmd}?')
+            ans = sys.stdin.read() # can't get this to work.  EOFError when using `input()`.
+            if ans == 'y' or ans == 'Y':
+                xargs(ex.pop(0))
+            else:   
+                continue
+    # otherwise, just call xargs
+    else:
+        for cmd in ex:
+            xargs(ex.pop(0))
 
-    xargs(args)
 
 if __name__ == '__main__':
     main()
