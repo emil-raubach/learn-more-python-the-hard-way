bb43eff0ebb5a4610fa3f26dd1c43ff61fa3dabb
Emil Raubach <eraubach@gmail.com>
2019-10-07 22:16:26 -0600

    Decent version of ex28 - sh with mocks

---
+++ ex28/sh.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import sys
+import subprocess
+
+# Breaking the io out as its own function 
+# so it can be replaced with a lambda in the test.
+def input_func():
+    user_input = input('> ')
+    return user_input
+
+def parse_input(input_func):
+    in_str = input_func()
+    parsed_str = in_str.strip().split(' ')
+    return parsed_str
+
+def run_process(command):
+    if command[0] == 'exit':
+        sys.exit() # had `break` before since this was in a while-loop.
+    else:
+        status = subprocess.run(command)
+
+def main():
+    try:
+        while True:
+            input_str = parse_input(input_func)
+            run_process(input_str)
+    except (EOFError, KeyboardInterrupt) as e:
+        print('\nbye!')
+
+
+if __name__ == "__main__":
+    main()
--- ex28/sh_spike.py
+++ ex28/sh_spike.py
@@ -1,11 +1,19 @@
 import subprocess
+import readline
 
+# readline.parse_and_bind('tab: complete')
+# readline.parse_and_bind('set editing-mode vi')
 
-while True:
-    user_input = input('> ')
-    command = user_input.strip().split(' ')
-    print("command is=", command)
-    if command[0] == 'exit':
-        break
-    else:
-        status = subprocess.run(command)
+# could even wrap this in a function... not sure the benefit.
+# 'cause it could then be tested of course!
+try:
+    while True:
+        user_input = input('> ')
+        command = user_input.strip().split(' ')
+        # print("command is=", command)
+        if command[0] == 'exit':
+            break
+        else:
+            status = subprocess.run(command)
+except (EOFError, KeyboardInterrupt) as e:  # stole this - pretty sweet!
+    print('\nShutting down...')
